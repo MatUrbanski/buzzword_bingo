24b0c008c5379b54c1b8e285ad67a567ef763f53
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2018-12-10 22:38:22 +0100

    Add Player struct

---
+++ bingo/lib/bingo/player.ex
@@ -0,0 +1,13 @@
+defmodule Bingo.Player do
+  @enforce_keys [:name, :color]
+  defstruct     [:name, :color]
+
+  alias Bingo.Player
+
+  @doc """
+  Creates a player with the given `name` and `color`.
+  """
+  def new(name, color) do
+    %Player{name: name, color: color}
+  end
+end
---
+++ bingo/test/bingo/player_test.exs
@@ -0,0 +1,16 @@
+defmodule Bingo.PlayerTest do
+  use ExUnit.Case, async: true
+
+  alias Bingo.Player
+
+  describe "new/2" do
+    test "creates a Player" do
+      name   = "Mat"
+      color  = "Green"
+      player = Player.new(name, color)
+
+      assert player.name  == "Mat"
+      assert player.color == "Green"
+    end
+  end
+end
