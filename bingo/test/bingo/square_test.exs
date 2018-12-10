5e1b61cc36aa26923a536da46daf3fe3aa657892
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2018-12-10 22:35:48 +0100

    Add Square struct

---
+++ bingo/test/bingo/square_test.exs
@@ -0,0 +1,26 @@
+defmodule Bingo.SquareTest do
+  use ExUnit.Case, async: true
+
+  alias Bingo.Square
+
+  describe "new/2" do
+    test "creates a square from a buzzword" do
+      phrase = "A"
+      points = 10
+      square = Square.new(phrase, points)
+
+      assert square.phrase == "A"
+      assert square.points == 10
+    end
+  end
+
+  describe "from_buzzword/1" do
+    test "creates a square from a buzzword" do
+      buzzword = %{phrase: "A", points: 10}
+      square   = Square.from_buzzword(buzzword)
+
+      assert square.phrase == "A"
+      assert square.points == 10
+    end
+  end
+end
--- bingo/test/bingo_test.exs
+++ bingo/test/bingo_test.exs
@@ -1,8 +1,3 @@
 defmodule BingoTest do
   use ExUnit.Case
-  doctest Bingo
-
-  test "greets the world" do
-    assert Bingo.hello() == :world
-  end
 end
