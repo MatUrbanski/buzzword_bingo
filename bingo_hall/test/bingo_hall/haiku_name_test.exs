957469cc8edb0c80a76ba5eefac3d339b470c201
Mateusz Urbański <mateuszurbanski@yahoo.pl>
2018-12-17 23:05:47 +0100

    Add BingoHall.HaikuName module

---
+++ bingo_hall/test/bingo_hall/haiku_name_test.exs
@@ -0,0 +1,13 @@
+defmodule BingoHall.HaikuNameTest do
+  use ExUnit.Case, async: true
+
+  alias BingoHall.HaikuName
+
+  describe "generate/1" do
+    test "Generates a unique, URL-friendly name" do
+      name = HaikuName.generate
+
+      assert String.length(name) > 0
+    end
+  end
+end
